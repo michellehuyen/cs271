4e536b7552315db287142cab2c7c404d0a942c85
michellehuyen <nguyemi4@oregonstate.edu>
2022-04-26 22:50:39 -0700

    fill.asm

--- project4/fill/Fill.asm
+++ project4/fill/Fill.asm
@@ -11,8 +11,74 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-
 (KEYBOARD_CHECK)
+@KBD // 24576
+D = M // D = M[24576] D = value at RAM[24576]
+@WHITE
+D;JEQ // if D = 0 then jump to WHITE else continue to BLACK
+
+@BLACK
+0;JMP 
+
+(WHITE)
+@SCREEN // 16384
+D = A // D = 16384
+
+@0
+M = D // M[0] = 16384
+
+(WHITE_LOOP)
+@0
+D = M // D = M[0] -> D = 16384
+
+A = D // A = 16384
+M = 0
+
+D = D + 1 // D = 16385
+
+@0
+M = D // M[0] = 16385
+
+@24576
+D = D - A // D = 18191
+
+@KEYBOARD_CHECK
+D;JEQ // if D = 0 jump to KEYBOARD_CHECK
+
+@WHITE_LOOP
+0;JMP
+
+
+
+(BLACK)
+@SCREEN
+D = A
+
+@0
+M = D
+
+(BLACK_LOOP)
+@0
+D = M
+
+A = D
+M = -1
+
+D = D + 1
+
+@0
+M = D
+
+@24576
+D = D - A 
+
+@KEYBOARD_CHECK
+D;JEQ
+
+@BLACK_LOOP
+0;JMP
+
+
 
 (EXIT)
 @EXIT
